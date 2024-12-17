8d1ff2cba585e5dc3aef17a3660cc5b2504f7ddd
Sylwester Sojka <sojka.sylwester.dev@gmail.com>
2024-12-17 16:34:52 +0100

    Initial working app.py version

--- app.py
+++ app.py
@@ -29,11 +29,132 @@ if not st.session_state.get("openai_api_key"):
     else:
         
         st.info("Please provide Your OPENAI API KEY")
-        st.session_state["openai_api_key"] = st.text_input("OPENAI API KEY:")
+        st.session_state["openai_api_key"] = st.text_input("OPENAI API KEY:", type="password")
         if st.session_state["openai_api_key"]:
             st.rerun()
 
     if not st.session_state.get("openai_api_key"):
         st.stop()
 
-st.file_uploader(label="Upload Portfolio screens", accept_multiple_files=True)
+openai_client = OpenAI(api_key=st.session_state.get("openai_api_key"))
+
+# Prompt definition
+CUMMULATED_PROMPT ="""
+Twoim zadaniem bedzie rozpoznanie obrazu na podstawie slowa kluczowego. Na tej podstawie wykonasz odpowiednia instrukcje. Zaczynamy:
+
+Jesli na obrazie zobaczysz napis 'Obligacje' wykonasz nastepujace polecenie: Na podstawie screenu pobierz dane na temat zainwestowanych obligacji. Kolumna odsteki pokazuje wypracowany zysk.
+Kolumna liczba przedstawia zakupione obligacje. 1 obligacja to 100zl.
+Dane przedstaw w formacie JSON w nastepujacym formacie
+{
+    "Obligacje": {
+        "Wartosc": "30000PLN",
+        "Zysk": "400PLN"
+    }
+}
+ {"Wartosc": 30000PLN, "Zysk": 400PLN (zysk musi byc zsumowany z kazdej emisji)}
+
+Jesli na obrazie bedzie napis Generali Investments wykonaj nastepujace polecenie:
+Na podstawie screenu pobierz dane na temat ogolnej wartosci rachunku oraz bilansu.
+Dane przedstaw w formacie JSON - bez komentarzy same dane w nastepujacym formacie:
+{
+    "GEnerali": {
+        "Wartosc": "30000PLN",
+        "Zysk": "400PLN"
+    }
+}
+
+Jesli na obrazie zobaczysz nazwe BITCOIN wykonasz nastepujace polecenie:
+Na podstawie screenu pobierzesz 2 wartosci liczbowe. 
+Pierwsza widoczna i najwieksza zapisana bedzie jako wartosc - znajdziesz ja na gorze obrazu pogrubiona czacionka pod paskiem 'Szukaj'. 
+Druga jako zysk (bedzie miala znaczek + z przodu).
+Dane zapiszesz w formacie JSON: 
+{
+    "Kryptowaluty": {
+        "Wartosc": "30000PLN", (- znajdziesz ja na gorze obrazu pogrubiona czacionka pod paskiem 'Szukaj')
+        "Zysk": "400PLN"
+    }
+}
+
+"""
+
+# Path locations
+RAW_IMAGE_FOLDER = Path("Images")
+PROCESSED_IMAGES = RAW_IMAGE_FOLDER / "Processed"
+
+for image_path in RAW_IMAGE_FOLDER.glob("*"):
+    st.write(image_path)
+
+
+# Prepreration file for openai
+
+def prep_file_for_openai(file_name, file_bytes):
+    # Sprawdzenie MIME typu na podstawie rozszerzenia pliku
+    mime_type, _ = mimetypes.guess_type(file_name)
+    
+    # Obsługiwane typy obrazów
+    supported_types = ["image/png", "image/jpeg"]
+    
+    # Sprawdzenie, czy typ MIME jest obsługiwany
+    if mime_type not in supported_types:
+        raise ValueError(f"Nieobsługiwany format obrazu: {mime_type}. Obsługiwane formaty to: {', '.join(supported_types)}")
+    
+    # Wczytanie obrazu i zakodowanie go w Base64
+
+    image_data = base64.b64encode(file_bytes).decode('utf-8')
+    return f"data:{mime_type};base64,{image_data}"
+
+
+# Openai Function
+def generate_ai_response(file_name, file_bytes):
+
+    encoded_image = prep_file_for_openai(file_name, file_bytes)
+
+    response = openai_client.chat.completions.create(
+        
+        model="gpt-4o",
+        temperature=0,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": CUMMULATED_PROMPT
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": encoded_image,
+                            "detail": "high"
+                        },
+                    },
+                ],
+            }
+        ],
+    )
+
+
+    return (response.choices[0].message.content)
+
+uploaded_files = st.file_uploader(
+    label="Upload Portfolio screens", 
+    accept_multiple_files=True,
+    type=["png", "jpg", "jpeg"]
+    )
+
+
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        st.write(f"Processing file: **{uploaded_file.name}**")
+
+        file_bytes = uploaded_file.read()
+        
+
+        with st.spinner("Generatin AI response..."):
+            try:
+                ai_response = generate_ai_response(uploaded_file.name, file_bytes)
+                st.success("Response generated!")
+                st.write("**AI Response:**")
+                st.write(ai_response)
+            except Exception as e:
+                st.error(f"Error: {e}")
